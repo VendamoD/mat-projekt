c4960d3b3d08c25cf565a27978569fe1e6018243
VendamoD <vendamod@post.cz>
2023-03-15 01:46:00 +0100

    bezici verze

--- components/Achievements.tsx
+++ components/Achievements.tsx
@@ -29,11 +29,11 @@ export function Achievements() {
       <HStack spacing="1rem" textAlign="center" display={"flex"} margin={5} borderWidth='1px' borderRadius='lg' overflow='hidden'>
         {
           //@ts-ignore
-          userData ? userData.SaberPuzzle ? <Card><Image width="40vw" src="trophy.png"></Image><Text fontSize={{ base: "1.1rem" }}>Světelné meče</Text></Card> : "" : ""
+          userData ? userData.SaberPuzzle ? <Card><Image width="20vw" src="trophy.png"></Image><Text fontSize={{ base: "1.1rem" }}>Světelné meče</Text></Card> : "" : ""
         }
         {
           //@ts-ignore
-          userData ? userData.WolfAndGoatPuzzle ? <Card><Image width="40vw" src="trophy.png"></Image><Text fontSize={{ base: "1.1rem" }}>Vlk, koza a zelí</Text></Card> : "" : ""
+          userData ? userData.WolfAndGoatPuzzle ? <Card><Image width="20vw" src="trophy.png"></Image><Text fontSize={{ base: "1.1rem" }}>Vlk, koza a zelí</Text></Card> : "" : ""
         }
         {
           //@ts-ignore
