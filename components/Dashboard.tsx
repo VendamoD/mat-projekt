a63497066eea676391871d92dd8272d1ed9795dc
VendamoD <vendamod@post.cz>
2023-03-15 01:11:18 +0100

    test verze

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -35,7 +35,7 @@ export function Dashboard() {
 
   useEffect(() => {
     getUserData()
-  })
+  }, [])
 
   return (
     <>
--- components/Home.tsx
+++ components/Home.tsx
@@ -20,7 +20,7 @@ export function Homepage() {
 
   useEffect(() => {
     getUserData()
-  })
+  }, [])
 
   return (
     <>
